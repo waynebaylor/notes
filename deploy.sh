5d9406fb9f0dd1464cfc66c701661063b9b94a13
Wade Taylor <wade.taylor@baylorsc.com>
2013-12-20 12:32:38 -0500

    moving property values to external file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
+export CATALINA_OPTS=-Dnotes.config=/home/wtaylor/tomcat/apache-tomcat-7.0.37/webapps/notes.properties
 ~/tomcat/apache-tomcat-7.0.37/bin/shutdown.sh; 
 mvn clean install;
-rm -rf ~/tomcat/apache-tomcat-7.0.37/work/Catalina/localhost/notes; 
-rm -rf ~/tomcat/apache-tomcat-7.0.37/webapps/notes*; 
+rm -rf ~/tomcat/apache-tomcat-7.0.37/work/Catalina/localhost/notes;
+rm ~/tomcat/apache-tomcat-7.0.37/webapps/notes.war; 
+rm -rf ~/tomcat/apache-tomcat-7.0.37/webapps/notes/; 
 cp target/notes.war ~/tomcat/apache-tomcat-7.0.37/webapps/; 
 ~/tomcat/apache-tomcat-7.0.37/bin/startup.sh
