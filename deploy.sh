14c94c1cbf798339cbbc761c18bbfc8186cc3130
Stephanie <stephanephanon@gmail.com>
2013-12-29 15:11:01 -0500

    .gitignore updated to remove deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-/usr/local/apache-tomcat-7.0.42/bin/shutdown.sh; 
-mvn clean install;
-rm -rf /usr/local/apache-tomcat-7.0.42/work/Catalina/localhost/notes; 
-rm -rf /usr/local/apache-tomcat-7.0.42/webapps/notes*; 
-cp target/notes.war /usr/local/apache-tomcat-7.0.42/webapps/; 
-/usr/local/apache-tomcat-7.0.42/bin/startup.sh
